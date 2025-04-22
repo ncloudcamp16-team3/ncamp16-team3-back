188a129832fc83fc35b7cf44284b85458acfd224
김태현 <kokiyo1030@naver.com>
2025-04-22 16:38:21 +0900

    [FEAT] 펫시터리스트 applyAt 조건 추가

--- src/main/java/tf/tailfriend/petsitter/repository/PetSitterDao.java
+++ src/main/java/tf/tailfriend/petsitter/repository/PetSitterDao.java
@@ -1,7 +1,13 @@
 package tf.tailfriend.petsitter.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tf.tailfriend.petsitter.entity.PetSitter;
 
 public interface PetSitterDao extends JpaRepository<PetSitter, Integer> {
+
+    Page<PetSitter> findByApplyAtIsNotNull(Pageable pageable);
+
+    Page<PetSitter> findByApplyAtIsNull(Pageable pageable);
 }

cc12649e63975ee64788b822f07edbd1ed381329
juyoung <eunh9372@naver.com>
2025-04-22 15:17:13 +0900

    [MODIFY]Member->User로 바꾸기

--- src/main/java/tf/tailfriend/user/service/UserService.java
+++ src/main/java/tf/tailfriend/user/service/UserService.java
@@ -24,7 +24,6 @@ import tf.tailfriend.user.repository.UserDao;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 

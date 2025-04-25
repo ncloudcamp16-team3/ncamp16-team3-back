0499dfaa0b595cc11fb3ee991e03dbedfecd3543
pgw30508 <pgw30508@daum.net>
2025-04-25 12:03:34 +0900

    .

--- src/main/java/tf/tailfriend/petsta/controller/PetstaController.java
+++ src/main/java/tf/tailfriend/petsta/controller/PetstaController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import tf.tailfriend.global.config.UserPrincipal;
-//import tf.tailfriend.global.service.RedisService;
 import tf.tailfriend.global.service.RedisService;
 import tf.tailfriend.petsta.entity.dto.PetstaFollowingUserDto;
 import tf.tailfriend.petsta.entity.dto.PetstaUserpageResponseDto;

96134a4d6b23673a071965261bec657ab3032223
pgw30508 <pgw30508@daum.net>
2025-04-26 10:46:08 +0900

    [FEAT] 알람 테스트

--- src/main/java/tf/tailfriend/notification/controller/NotificationController.java
+++ src/main/java/tf/tailfriend/notification/controller/NotificationController.java
@@ -1,4 +1,21 @@
 package tf.tailfriend.notification.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tf.tailfriend.notification.entity.dto.NotificationDto;
+import tf.tailfriend.notification.service.NotificationService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/test")
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @PostMapping("/notify")
+    public ResponseEntity<?> notifyUser(@RequestBody NotificationDto notificationDto) {
+        notificationService.sendNotification(notificationDto);
+        return ResponseEntity.ok("알림 전송 완료");
+    }
 }

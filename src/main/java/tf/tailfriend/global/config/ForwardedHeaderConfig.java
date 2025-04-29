3ed70a99291cb52c4a4d6f9dfcc3e00f56596292
taesun <taesun4767@gmail.com>
2025-04-29 16:49:22 +0900

    test

--- src/main/java/tf/tailfriend/global/config/ForwardedHeaderConfig.java
+++ src/main/java/tf/tailfriend/global/config/ForwardedHeaderConfig.java
@@ -1,5 +1,6 @@
 package tf.tailfriend.global.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
@@ -8,7 +9,10 @@ import org.springframework.web.filter.ForwardedHeaderFilter;
 public class ForwardedHeaderConfig {
 
     @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new ForwardedHeaderFilter());
+        filter.setOrder(0); // ✅ 필수
+        return filter;
     }
 }

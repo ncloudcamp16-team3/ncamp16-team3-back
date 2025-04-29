df410a69c8fa1043d84f054f1ed9c7702f4f8bde
taesun <taesun4767@gmail.com>
2025-04-29 17:00:40 +0900

    test

--- src/main/java/tf/tailfriend/global/config/SessionConfig.java
+++ src/main/java/tf/tailfriend/global/config/SessionConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
+
 @Configuration
 public class SessionConfig {
 
@@ -12,10 +13,11 @@ public class SessionConfig {
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setCookieName("SESSION");
-        serializer.setDomainName("tailfriends.kro.kr"); // . 제거 주의!
-        serializer.setUseSecureCookie(true); // HTTPS면 true
-        serializer.setSameSite("Lax");
+        serializer.setDomainName("tailfriends.kro.kr"); // 또는 ".tailfriends.kro.kr" → 둘 다 실험해보기
+        serializer.setCookiePath("/");
+        serializer.setSameSite("Lax"); // 또는 "None" (프론트 상황에 따라)
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setUseSecureCookie(true); // HTTPS만 쓸 거면 true
         return serializer;
     }
-
 }

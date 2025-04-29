eee25fbf4e54c3bf8f211fc3a10b1c52069cba51
taesun <taesun4767@gmail.com>
2025-04-29 16:38:33 +0900

    test

---
+++ src/main/java/tf/tailfriend/global/config/ForwardedHeaderConfig.java
@@ -0,0 +1,14 @@
+package tf.tailfriend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class ForwardedHeaderConfig {
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+}

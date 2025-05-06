c954320b44a60fee87ac662bad20d4dae228a8d8
jowanghui@gmail.com <jowanghui@gmail.com>
2025-05-06 22:57:14 +0900

    [REFACTOR] db.migration

--- src/main/resources/db/migration/V6__review_photos.sql
+++ src/main/resources/db/migration/V6__review_photos.sql
@@ -1,7 +1,5 @@
-wq(
-    file_id     INTEGER NOT NULL, -- 파일아이디
+file_id     INTEGER NOT NULL, -- 파일아이디
     review_id INTEGER NOT NULL, -- 리뷰 아이디
     PRIMARY KEY (file_id, review_id),
     FOREIGN KEY (review_id) REFERENCES reviews (id),
     FOREIGN KEY (file_id) REFERENCES files (id)
-);
